d8271932483defaa305e1c8de4c42f639835b658
Carlos Ravelo <ravelo.carlos@gmail.com>
2014-01-08 23:33:50 -0500

    Red stripe animation and misc

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+/*global jQuery, impress */
 
 (function($){
-    $(document).ready(
-        function(){
-            impress().init();
-        }
-    );
+    $(document).ready(function(){
+        impress().init();
+
+        $("#intro .bottom-text .horizontal-stripe").width(300);
+        $("#intro .bottom-text .red-arrow-right").css('left', 295);
+        setTimeout(function(){
+            $("#intro .bottom-text .red-arrow-right").width(20);
+            setTimeout(function(){
+                $("#intro .bottom-text .red-arrow-right").css('-webkit-animation-name', 'elastic');
+            }, 200);
+        }, 1000);
+    });
 }(jQuery))
